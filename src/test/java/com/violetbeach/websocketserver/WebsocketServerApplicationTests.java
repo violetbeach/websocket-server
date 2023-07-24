8740f3779b009ceffc5de13434a9cfa214d5ee9a
Lee Jaeheon <ti6419@gmail.com>
2023-07-24 23:19:18 +0900

    Initial commit

---
+++ src/test/java/com/violetbeach/websocketserver/WebsocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.violetbeach.websocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
